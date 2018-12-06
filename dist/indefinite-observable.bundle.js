de36cd113e75903c89671c4894f334d84095ae1c
Brenton Simpson <appsforartists@google.com>
2018-12-05 19:18:14 -0800

    [added] `browser` and `module` bundles
    Summary:
    Previously, `indefinite-observable.js` was a script that wrote `IndefiniteObservable` to the global scope.  Now, it's a module that exports `IndefiniteObservable` and imports `symbol-observable`.
    
    The previous `indefinite-observable.js` has been replaced with `indefinite-observable.bundle.js`.  It still inlines `symbol-observable`, but also exports `IndefiniteObservable` rather than writing to the global scope.
    
    Reviewers: O2 Material Motion, O3 Material JavaScript platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3496

--- dist/indefinite-observable.bundle.js
+++ dist/indefinite-observable.bundle.js
@@ -1,3 +1,63 @@
+/** @license for symbol-observable
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (sindresorhus.com)
+ *  Copyright (c) Ben Lesh <ben@benlesh.com>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+function symbolObservablePonyfill(root) {
+	var result;
+	var Symbol = root.Symbol;
+
+	if (typeof Symbol === 'function') {
+		if (Symbol.observable) {
+			result = Symbol.observable;
+		} else {
+			result = Symbol('observable');
+			Symbol.observable = result;
+		}
+	} else {
+		result = '@@observable';
+	}
+
+	return result;
+}
+
+/* global window */
+
+var root;
+
+if (typeof self !== 'undefined') {
+  root = self;
+} else if (typeof window !== 'undefined') {
+  root = window;
+} else if (typeof global !== 'undefined') {
+  root = global;
+} else if (typeof module !== 'undefined') {
+  root = module;
+} else {
+  root = Function('return this')();
+}
+
+var $observable = symbolObservablePonyfill(root);
+
 /** @license
  *  Copyright 2016 - present The Material Motion Authors. All Rights Reserved.
  *
@@ -13,8 +73,26 @@
  *  License for the specific language governing permissions and limitations
  *  under the License.
  */
-import $$observable from 'symbol-observable';
-import wrapWithObserver from './wrapWithObserver';
+/**
+ * TypeScript is a pain to use with polymorphic types unless you wrap them in a
+ * function that returns a single type.  So, that's what this is.
+ *
+ * If you give it an observer, you get back that observer.  If you give it an
+ * anonymous function, you get back that anonymous function wrapped in an
+ * observer.
+ */
+function wrapWithObserver(listener) {
+    if (typeof listener === 'function') {
+        return {
+            next: listener
+        };
+    }
+    else {
+        return listener;
+    }
+}
+
+
 /**
  * `Observable` is a standard interface that's useful for modeling multiple,
  * asynchronous events.
@@ -23,7 +101,7 @@ import wrapWithObserver from './wrapWithObserver';
  * Observable proposal.  It is indefinite because it will never call `complete`
  * or `error` on the provided observer.
  */
-export class IndefiniteObservable {
+class IndefiniteObservable {
     /**
      * The provided function should receive an observer and connect that
      * observer's `next` method to an event source (for instance,
@@ -67,9 +145,12 @@ export class IndefiniteObservable {
      *
      * https://github.com/tc39/proposal-observable#observable
      */
-    [$$observable]() {
+    [$observable]() {
         return this;
     }
 }
+
+
+
+export { wrapWithObserver, IndefiniteObservable };
 export default IndefiniteObservable;
-//# sourceMappingURL=IndefiniteObservable.js.map
